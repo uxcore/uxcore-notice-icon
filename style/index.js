5abcf6d5d72e55cb23ccc377cfd725b491eac4f2
光弘 <guanghong.wsj@alibaba-inc.com>
2018-10-31 11:49:57 +0800

    ver. 0.1.23

--- style/index.js
+++ style/index.js
@@ -1,6 +1,6 @@
-import '../src/NoticeIcon.less';
 import 'uxcore-badge/style';
 import 'uxcore-button/style';
 import 'uxcore-empty-data/style';
 import 'uxcore-icon/style';
 import 'uxcore-popover/style';
+import '../src/NoticeIcon.less';
